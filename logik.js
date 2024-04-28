28485615fa4d6fd93b55601251cabbb95536b8a8
Mikkel Gissel <mikkel@gizzel.eu>
2024-04-28 16:27:36 +0200

    wonders of js

--- logik.js
+++ logik.js
@@ -1,4 +1,23 @@
-document.ready
-const form = document.getElementById('base-nums')
+docReady(function() {
+    // DOM is loaded and ready for manipulation here
+    const form = document.getElementById('base-nums')
+    form.style.background = '#555'
+    form.addEventListener('submit', (event)=>{
+        event.preventDefault()
+        const inputData = new FormData(form)
+        for(var i =0; i < inputData.entries.length -1; i++){
+            console.log('form element val: ', inputData.entries[i])
+        }
+    })
+})
 
-form.style.background = '#555'
+
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}  
