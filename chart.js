a00d78d709274e875e283310ef56d3f8864f3e3c
Mikkel Gissel <mikkel@gizzel.eu>
2024-05-05 00:24:39 +0200

    cleanup and stylish styling

--- chart.js
+++ chart.js
@@ -14,7 +14,7 @@ class Chart{
 	      // linesAcross & right side intervals
 	  var _text = 1
 	  for(let j = 20; j <= 1020; j+= 20){
-	  ctx.strokeStyle = 'rgba(80, 80, 80, 0.4)'
+	  ctx.strokeStyle = 'rgba(80, 80, 80, 0.6)'
 		  ctx.beginPath()
 		  let x = 0
 		  let y = j
@@ -24,10 +24,10 @@ class Chart{
 		  ctx.closePath()
 		  // text
 		  this.ctx.strokeStyle = 'rgba(80, 80, 80, 1)'
-		  ctx.strokeText(`${_text.toFixed(2)}`, (canvas.width - 30), (y - 10))
+		  ctx.strokeText(`${_text.toFixed(2)}`, (canvas.width - 30), (y - 2))
 			  _text -= 0.02
 		}
-	  // Lines down & datapoint
+	  // Lines down
       const cellsWidth = Math.floor(canvas.width / dataPoints.length)
 	  var currentPoint = {x: 0, y: 0}
 	  var previousPoint = {x: 0, y: 0}
@@ -35,13 +35,8 @@ class Chart{
 		 ctx.strokeStyle = 'rgba(80, 80, 80, 0.4)'
          ctx.beginPath()
          let x = 0
-         // if (i > dataPoints.length) {
-            // x = Math.floor(((i * cellsWidth)- margin.x))
-         // }else{
             x = Math.floor((i * cellsWidth))
-         // }
          let y = margin.y
-         console.log('x and y', x, y)
          ctx.moveTo(x, y)
          y = Math.floor((canvas.height - margin.y))
          console.log('x and y', x, y)
@@ -52,16 +47,18 @@ class Chart{
 		  if(i <= dataPoints.length -1){
 			  currentPoint.x = (x + (cellsWidth / 2))
 			  currentPoint.y = ( Math.floor((canvas.height - (dataPoints[i] * 1000)- margin.y)))
+			  const r_g_b = (255 * dataPoints[i])
+			  // circles
 			  ctx.beginPath()
 				ctx.arc(currentPoint.x, currentPoint.y, 3, 0, 2 * Math.PI)
 				ctx.fillStyle = `rgb(255 255 255/ ${Math.floor((dataPoints[i].toFixed(2)*100) )}%)`
 				ctx.fill()
 				ctx.linewidth = 1
-				ctx.strokestyle = 'rgb(255, 255, 255/ 100%)'
+				ctx.strokestyle = 'rgb(r_g_b, r_g_b, r_g_b/ 100%)'
 				ctx.stroke()
 				ctx.closePath()
-				//if(previousPoint.x != 0){
-					ctx.strokestyle = 'rgb(255, 255, 255)'
+				// lines between circles
+					ctx.strokestyle = 'rgb(r_g_b, r_g_b, r_g_b)'
 					ctx.linewidth = 2
 					ctx.beginPath()
 					ctx.moveTo(previousPoint.x, previousPoint.y)
@@ -70,11 +67,7 @@ class Chart{
 					ctx.closePath()
 					previousPoint.x = currentPoint.x
 					previousPoint.y = currentPoint.y
-					console.log('line drawn', currentPoint.x)
-				//}
          }
-         console.log('chart drawn', canvas)
-    
     // text bottom
 	
 	  }
